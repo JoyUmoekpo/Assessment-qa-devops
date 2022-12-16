6d9234eb4b8d5faf9fe898ce8140a91009813e63
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-16 15:47:30 -0600

    Added path variable and endpoint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+}) 
 
 app.get('/api/robots', (req, res) => {
     try {
