9e3c97f6286aa5932d0a5dcad3401b33642de71c
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-17 20:52:07 -0600

    Added Rollbar intialization to server and .env to gitignore

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors');
 const app = express()
 const {bots, playerRecord} = require('./data')
 const {shuffleArray} = require('./utils')
 const path = require('path')
+const {ROLLBAR_TOKEN} = process.env
 
 app.use(express.json())
 app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: ROLLBAR_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'))
