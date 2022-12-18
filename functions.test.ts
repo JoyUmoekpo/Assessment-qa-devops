2dbb8e282f546bb71b203854098d2cf8582fa87c
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-17 18:13:15 -0600

    Completed Part 3: Unit Tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,17 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+
+    test('returns an array', () => {
+        let array = ['apple', 'pan', 'milk']
+        let result = shuffleArray(array)
+        expect(result).toHaveProperty('length')
+    })
+
+    test('returns an array of the same length as the argument sent in', () => {
+        let array = ['apple', 'pan', 'milk']
+        let result = shuffleArray(array)
+        expect(result.length).toEqual(3)
+    })
+
 })
