fe83a1ec0958c8ea335432d39115b14fdd5442fa
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-17 18:22:44 -0600

    Edited 2nd Unit Test

--- functions.test.ts
+++ functions.test.ts
@@ -2,16 +2,15 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
 
-    test('returns an array', () => {
-        let array = ['apple', 'pan', 'milk']
-        let result = shuffleArray(array)
-        expect(typeof result).toHaveProperty('length')
-    })
-
     test('returns an array of the same length as the argument sent in', () => {
         let array = ['apple', 'pan', 'milk']
         let result = shuffleArray(array)
         expect(result).toHaveLength(3)
     })
-
+    
+    test('returns an array', () => {
+        let array = ['apple', 'pan', 'milk']
+        let result = shuffleArray(array)
+        expect(typeof result).toHaveProperty('length')
+    })
 })
