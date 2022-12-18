230b7824292eb135095758994f68afa2f0cbf9b4
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-17 20:39:25 -0600

    Edited test messages

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -13,30 +13,25 @@ afterAll(async () => {
     driver.quit()
 })
 
-describe('Duel Duo Test to check that ', () => {
-    test('Title shows up when page loads', async () => {
-        const title = await driver.findElement(By.id('title'))
-        const displayed = await title.isDisplayed()
-        expect(displayed).toBe(true)
-    })
-    
-    test('Clicking an “Add to Duo” button displays the div with id player-duo',
-    async() => {
-        await driver.findElement(By.id('draw')).click()
-        await driver.sleep(7000)
-        await driver.findElement(By.xpath('(//button[text() = "Add to Duo"])[1]')).click()
-        const userDuo = await driver.findElement(By.id('player-duo'))
-        const display = await userDuo.isDisplayed()
-        expect(display).toBeTruthy()
-    })
-    
-    test('Clicking on the draw button displays the div with id choices',
-    async() => {
-        await driver.findElement(By.id('draw')).click()
-        await driver.sleep(7000)
-        const choiceContainer = await driver.findElement(By.id('choices'))
-        const display = await choiceContainer.isDisplayed()
-        expect(display).toBeTruthy()
-    })
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
 
+it('clicks an “Add to Duo” button and displays the div with id player-duo',async() => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(4000)
+    await driver.findElement(By.xpath('(//button[text() = "Add to Duo"])[1]')).click()
+    const userDuo = await driver.findElement(By.id('player-duo'))
+    const display = await userDuo.isDisplayed()
+    expect(display).toBeTruthy()
+})
+
+it('clicks on the draw button and displays the div with id choices', async() => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(4000)
+    const choiceContainer = await driver.findElement(By.id('choices'))
+    const display = await choiceContainer.isDisplayed()
+    expect(display).toBeTruthy()
 })
--- functions.test.ts
+++ functions.test.ts
@@ -2,15 +2,16 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
 
-    test('returns an array of the same length as the argument sent in', () => {
+    it('returns an array of the same length as the argument sent in', () => {
         let array = ['apple', 'pan', 'milk']
         let result = shuffleArray(array)
         expect(result).toHaveLength(3)
     })
 
-    test('returns an array', () => {
+    it('returns an array', () => {
         let array = ['apple', 'pan', 'milk']
         let result = shuffleArray(array)
         expect(result).toHaveProperty('length')
     })
+
 })
